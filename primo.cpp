27646a4456efa12da2fa1323dd16025964259330
Bruno Wada <bruno.wada00@gmail.com>
2025-07-03 01:10:27 -0300

    Padroniza comentarios para ASCII

--- primo.cpp
+++ primo.cpp
@@ -3,23 +3,23 @@
 
 using namespace std;
 
-// Função para verificar se um número é primo
-// Retorna 1 (verdadeiro) se o número for primo
-// Retorna 0 (falso) se o número não for primo
+// Funcao para verificar se um numero e primo
+// Retorna 1 (verdadeiro) se o numero for primo
+// Retorna 0 (falso) se o número nao for primo
 int Primo(int numero) {
     if (numero <= 1) {
         return 0;
     }
     if (numero % 2 == 0) {
         if (numero == 2) {
-            // É primo (o número 2)
+            // Eh primo (o numero 2)
             return 1; 
         } else {
-            // É par, mas não é 2, então não é primo
+            // Eh par, mas nao eh 2, então nao eh primo
             return 0; 
         }
     }
-    // Verifica divisores ímpares a partir de 3 até a raiz quadrada do número
+    // Verifica divisores impares a partir de 3 ate a raiz quadrada do numero
     for (int i = 3; i * i <= numero; i += 2) {
         if (numero % i == 0) {
             return 0;
@@ -33,13 +33,13 @@ int main()
     int num;
     cin >> num;
     
-    // Chama a função Primo() para verificar se 'num' é primo
-    // Se a função retornar 1 (verdadeiro)
+    // Chama a funcao Primo() para verificar se 'num' eh primo
+    // Se a funcao retornar 1 (verdadeiro)
     if (Primo(num) == 1) { 
         cout << "Primo" << endl;
-        // Se a função retornar 0 (falso)
+        // Se a funcao retornar 0 (falso)
         } else { 
-            cout << "Não é primo" << endl; // Imprime "Não é primo" na tela
+            cout << "Nao eh primo" << endl; // Imprime "Nao eh primo" na tela
             }
 
     return 0;
