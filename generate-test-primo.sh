13302ea6f70dfd7d5a48db72c11ac4959fbcbaf1
Bruno Wada <bruno.wada00@gmail.com>
2025-07-04 00:27:22 -0300

    Adiciona para ler a saida no script

--- generate-test-primo.sh
+++ generate-test-primo.sh
@@ -3,15 +3,16 @@
 ARQUIVO_IN="primo.in"
 ARQUIVO_OUT="primo.out"
 
-# Gera um número entre 1 e 10000
+# Gera número entre 1 e 10000
 NUM=$(( RANDOM % 10000 + 1 ))
 echo "$NUM" > "$ARQUIVO_IN"
 echo "Número gerado para teste: $NUM"
 
-# Executa o programa se o executável existir
+# Executa o programa, se existir
 if [[ -x ./primo ]]; then
     ./primo < "$ARQUIVO_IN" > "$ARQUIVO_OUT"
-    echo "Saída salva em $ARQUIVO_OUT"
+    echo "Saída gerada:"
+    cat "$ARQUIVO_OUT"      # Mostra a saída no terminal
 else
     echo "Executável 'primo' não encontrado. Compile com: make primo"
 fi
